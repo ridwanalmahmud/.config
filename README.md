7d71cc5fc536375c1aea17aa2da9104aa7153749
ridwanalmahmud <ridwanalmahmud107@gmail.com>
2025-05-30 15:35:11 +0000

    basic workflow configuration

---
+++ README.md
@@ -0,0 +1,55 @@
+## terminal conf
+
+- JetBrainsMono 
+```
+curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFont-Regular.ttf
+```
+
+- IosevkaTerm
+```
+curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/IosevkaTerm/IosevkaTermNerdFont-Regular.ttf
+```
+
+- Iosevka
+```
+curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/patched-fonts/Iosevka/IosevkaNerdFont-Regular.ttf
+```
+
+- command line tools
+```
+pacman -S man ufw clang gdb neovim git tmux zsh github-cli fzf neofetch make -y
+```
+
+## git conf
+
+```
+git config --global user.name "<username>"
+
+git config --global user.email "<email>"
+
+git config --global init.defaultBranch "master"
+```
+
+## neovim conf
+
+```
+git clone https://github.com/ridwanalmahmud/config.git
+```
+
+## zsh conf
+
+- oh my zsh
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+```
+```
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+```
+
+- .zshrc
+```
+for file in ~/.config/zsh/*.zsh
+do
+  source "$file"
+done
+```
---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,89 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui"
+        },
+        config = function()
+            local dap = require("dap")
+            dap.set_log_level("DEBUG")
+
+            vim.keymap.set('n', '<leader>dt', function() dap.toggle_breakpoint() end)
+            vim.keymap.set('n', '<leader>dc', function() dap.continue() end)
+            vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+            vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+            vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+            vim.keymap.set('n', '<leader>B', function() dap.set_breakpoint() end)
+
+            -- dap adapters
+            dap.adapters.gdb = {
+                type = "executable",
+                command = "gdb",
+                args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+            }
+
+            -- dap adaters configuration
+            dap.configurations.c = {
+                {
+                    name = "Launch",
+                    type = "gdb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = "${workspaceFolder}",
+                    stopAtBeginningOfMainSubprogram = false,
+                },
+                {
+                    name = "Select and attach to process",
+                    type = "gdb",
+                    request = "attach",
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    pid = function()
+                        local name = vim.fn.input('Executable name (filter): ')
+                        return require("dap.utils").pick_process({ filter = name })
+                    end,
+                    cwd = '${workspaceFolder}'
+                },
+                {
+                    name = 'Attach to gdbserver :1234',
+                    type = 'gdb',
+                    request = 'attach',
+                    target = 'localhost:1234',
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}'
+                },
+            }
+        end
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio"
+        },
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+
+            require("dapui").setup()
+
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+        end
+    }
+}
